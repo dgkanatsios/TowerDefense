f6b255fe13b6a35dd4010a289f8d9adb51e6a567
dgkanatsios <dgkanatsios@outlook.com>
2014-08-21 21:49:05 +0300

    Added AudioManager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+    public AudioClip ArrowAudioClip, DeathSoundAudioClip;
+
+    public void PlayArrow()
+    {
+        GameObject go = new GameObject("arrowSound");
+        go.AddComponent<AudioSource>();
+        go.GetComponent<AudioSource>().PlayOneShot(ArrowAudioClip);
+        Destroy(go, ArrowAudioClip.length);
+    }
+
+    public void PlayDeathSound()
+    {
+        GameObject go = new GameObject("deathSound");
+        go.AddComponent<AudioSource>();
+        go.GetComponent<AudioSource>().PlayOneShot(DeathSoundAudioClip);
+        Destroy(go, DeathSoundAudioClip.length);
+    }
+}
